8619043954d90fbfc3e0ddcbf0dc744460d83f0c
Jared Patterson <jpwesley34@gmail.com>
2023-11-01 06:15:05 -0700

    makes you special

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(action = "roller-skate") {
+    //if (action = "bathe my dog") {
+    return `This Saturday, I want to ${action}!`;
+    //}
+    //else {
+    //    return `This Saturday, I want to roller-skate!`;
+    //}
+}
+saturdayFun();
+
+//function mondayWork(job = "go to the office") {
+//    return `This Monday, I will ${job}.`;
+//};
+
+const mondayWork = function(job = "go to the office") {
+    return `This Monday, I will ${job}.`;
+}
+
+function wrapAdjective(flair = "~!*") {
+    return function(trait = "special") {
+        return `You are ${flair}${trait}${flair}!`;
+    }
+};
